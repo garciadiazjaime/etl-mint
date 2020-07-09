38613550ac648e104e91dc69e570c8d51793a01a
jaime <garciadiazjaime@gmail.com>
2020-07-08 20:34:16 -0500

    refactor: [feedme] get browser

--- utils/extract.js
+++ utils/extract.js
@@ -3,9 +3,8 @@ const debug = require('debug')('app:realstate');
 const fs = require('fs');
 const { promisify } = require('util');
 
-const puppeteer = require('puppeteer');
-
 const readFileAsync = promisify(fs.readFile);
+const { getBrowser } = require('./browser');
 const config = require('../config');
 
 async function extract(url, source, cookies) {
@@ -13,22 +12,14 @@ async function extract(url, source, cookies) {
     return readFileAsync(`./stubs/${source}.html`, { encoding: 'utf8' });
   }
 
-  const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4152.0 Safari/537.36';
-  const browser = await puppeteer.launch({
-    headless: true,
-    executablePath: '/usr/bin/chromium-browser',
-    args: [
-      '--no-sandbox',
-      '--disable-gpu',
-      '--headless',
-    ],
-  });
+  const browser = await getBrowser();
   const page = await browser.newPage();
 
   if (Array.isArray(cookies) && cookies.length) {
     await page.setCookie(...cookies);
   }
 
+  const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4152.0 Safari/537.36';
   await page.setUserAgent(userAgent);
 
   try {
