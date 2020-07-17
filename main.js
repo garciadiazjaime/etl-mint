46c8b35dc2b02da6184908ca5d3bf06d71d5cf79
jaime <garciadiazjaime@gmail.com>
2020-07-16 19:59:16 -0500

    feat: [feedme] adjust cron

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ function main() {
     await mapSeries(sites, realState);
   });
 
-  cron.schedule('29 17-23,0-4 * * *', instagramWorker);
+  cron.schedule('17,47 * * * *', instagramWorker);
 
   cron.schedule('13 18-23/2 * * *', async () => {
     await instagramScheduler();
