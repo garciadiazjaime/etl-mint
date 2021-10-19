75b44c3cf45ccb000478a50aebcf6b971a31e3b5
jaime <garciadiazjaime@gmail.com>
2021-10-19 16:23:01 -0500

    feat: try login twice

--- sites/instagram/login.js
+++ sites/instagram/login.js
@@ -4,7 +4,8 @@ const { getBrowser } = require('../../utils/browser');
 const config = require('../../config');
 
 async function main() {
-  const url = 'https://www.instagram.com/accounts/login/';
+  let url = 'https://www.instagram.com/accounts/login/';
+  debug(url);
 
   const browser = await getBrowser();
 
@@ -12,12 +13,26 @@ async function main() {
   const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4152.0 Safari/537.36';
   await page.setUserAgent(userAgent);
 
-  try {
+  await page.goto(url);
+
+  const html = await page.content();
+  await page.screenshot({ path: './public/login-01.png' });
+
+  debug(html);
+  if (html.includes('Please wait a few minutes before you try again')) {
+    debug('ERROR');
+
+    await page.screenshot({ path: './public/login-02.png' });
+
+    url = 'https://www.instagram.com/';
+
+    debug(url);
+
     await page.goto(url);
-    await page.screenshot({ path: './public/login.png' });
-  } catch (err) {
-    return debug(err.message);
+
+    await page.screenshot({ path: './public/login-03.png' });
   }
+
 
   await page.waitForSelector('form', { timeout: 1000 * 3 });
 
